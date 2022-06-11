97130b1f665c67409b6ccf15b15996b6d5076bab
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-06-11 17:10:57 +0300

    profile model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import datetime
+from email.policy import default
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField(default='default.jpg', upload_to='images/')
+    bio =models.TextField()
+    contact = models.CharField(max_length=22)
+
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(self):
+        self.save()
