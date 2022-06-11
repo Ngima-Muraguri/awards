e7393f86bd5b038895f2327646f88c066d210305
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-06-11 17:52:45 +0300

    tests

---
+++ myapp/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Projects, Profile
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ('id', 'title', 'image', 'description', 'live_link', 'posted_by', 'date_posted')
+
+
+        
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id', 'user', 'profile_pic', 'bio', 'contact' )
